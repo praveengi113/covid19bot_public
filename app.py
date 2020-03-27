157f9b968c0d8e92dc76e9ad2e8cfc647b383ea0
praveengi113 <praveenkumar.p113@gmail.com>
2020-03-27 07:32:27 +0530

    Copied all application files to the local repo

---
+++ app.py
@@ -0,0 +1,51 @@
+from threading import Thread, Event
+from statusInd import Status
+from datetime import datetime
+from pytz import timezone
+
+
+def covidBot():
+    time = datetime.now(timezone('Asia/Kolkata'))
+    time = time.strftime("%d/%m/%Y %H:%M:%S")
+    status = Status()
+    status_now = status.scraper()
+    statusCheck = status.status_check()
+
+    body = """Hi, This is COVID-19@IndBot
+    This is a simple bot to notify about
+    the COVID19 cases in India.
+    _Created By: Praveen_
+    *Data Source: MoHFW*
+   
+    *Status*
+    Status on = {0}
+    Confirmed cases = {1}
+    Cured cases = {2}
+    Total Deaths = {3}
+
+    _Be safe at home..._
+    _Save Humanity..._
+    """
+    mess = body.format(time, status_now[0], status_now[1], status_now[2])
+    if statusCheck:
+        status.save_to_file()
+        status.whatsapp(mess, "7418414006")
+        status.whatsapp(mess, "9790040925")
+    else:
+        print("{} --->  No Change".format(time))
+
+
+class MyThread(Thread):
+    def __init__(self, event):
+        Thread.__init__(self)
+        self.stopped = event
+
+    def run(self):
+        while not self.stopped.wait(1800):
+            covidBot()
+
+
+covidBot()
+stopFlag = Event()
+thread = MyThread(stopFlag)
+thread.start()
---
+++ statusInd.py
@@ -0,0 +1,69 @@
+from datetime import datetime
+import uuid
+import json
+import requests
+from bs4 import BeautifulSoup
+from pytz import timezone
+from twilio.rest import Client
+
+
+class Status(object):
+    def __init__(self, id=None, date=datetime.now(timezone('Asia/Kolkata'))):
+        self.cases = self.scraper()[0]
+        self.cured = self.scraper()[1]
+        self.death = self.scraper()[2]
+        self.id = uuid.uuid4().hex if id is None else id
+        self.time = date.strftime("%d/%m/%Y %H:%M:%S")
+
+    def save_to_file(self):
+        with open("src/data.json") as f:
+            data = json.load(f)
+            temp = data['status']
+            temp.append(self.json_upd)
+        self.write_json(data)
+
+    def write_json(self, data, filename='src/data.json'):
+        with open(filename, 'w') as f:
+            json.dump(data, f, indent=4)
+
+    @property
+    def json_upd(self):
+        return {
+            "cases": self.cases,
+            "cured": self.cured,
+            "death": self.death,
+            "id": self.id,
+            "time": self.time
+        }
+
+    def status_check(self):
+        with open("src/data.json") as f:
+            data = json.load(f)
+            temp = data['status']
+        temp = temp[-1]["cases"]
+        if int(temp) == int(self.scraper()[0]):
+            return False
+        else:
+            return True
+
+    def scraper(self):
+        url = "https://www.mohfw.gov.in/"
+        request = requests.get(url)
+        content = request.content
+        soup = BeautifulSoup(content, "html.parser")
+        elements = soup.findAll("span", {"class": "icount"})
+        status_var = []
+        for e in elements[1:]:
+            el = e.text.strip()
+            status_var.append(el)
+        return status_var
+
+    def whatsapp(self, body, num):
+        sid = "AC2f8aa150a9ea9a4397e8b247c2747c5d"
+        token = "ac3db843ff54606aacb81ddfbfe33d14"
+
+        client = Client(sid, token)
+        fr_num = "whatsapp:+14155238886"
+        to_num = "whatsapp:+91" + num
+
+        client.messages.create(body=body, from_=fr_num, to=to_num)
